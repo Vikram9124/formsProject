336dbb330726773c4b101e30c4528b3ee5a2acf7
Vikram9124 <143021423+Vikram9124@users.noreply.github.com>
2024-01-15 15:39:05 +0530

    Add files via upload

---
+++ main/java/com/spring/userlogin/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.userlogin;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+
+}
